be3fdbb140fc78914a8f782914857e2f3d2bdde9
lpybus <lpybus@Lawtons-MacBook-Air.local>
2014-07-14 13:44:33 -0400

    Removed an appendage
    Discovered an evolutionary appendage that was completely unnecessary
    (misunderstood the problem at first).

--- problem02.js
+++ problem02.js
@@ -7,7 +7,6 @@ PROJECT EULER: PROBLEM #2
 var fibonacci = [1, 2];
 var fibEvens = [];
 
-var fibSum = 3;     // Initialize the sum of the Fibonacci array (1 + 2 = 3)
 var fibNext = 0;    // Initialize the "next" Fibonacci number in the sequence (and the next two placeholder variables) at 0 (just so it's a number)
 var i = 0;
 var j = 0;
@@ -20,8 +19,6 @@ while (fibNext < 4000000) {
     fibNext = fibonacci[i] + fibonacci[j];
     
     fibonacci.push(fibNext);
-    
-    fibSum += fibNext;
 };
 
 // Get the even numbers out and into their own array.
